01c237392a23d839ace3fabd8bc706af10c26204
kngtee <anthonyurah@gmail.com>
2024-03-28 12:30:01 +0100

    services done

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -1,24 +1,41 @@
+import { useState } from "react";
+import { serviceItems } from "../constants";
+import { Button } from "./ui/button";
+
 export const OurServices = () => {
+  const [viewAll, setViewAll] = useState<boolean>(false);
   return (
     <section className="container h-fit">
       <div className="flex flex-col m-10 items-center justify-center">
-        <span className="text-sm text-gray-500">Our Services</span>
-        <div className="flex gap-4 mt-10">
-        <span className="text-[18px] leading-[28px] tracking-tight font-[400] text-start">
-          GreenLee Behavioral Health Services treats a diverse population of
-          children, teens, and adults who need treatment for behavioral and
-          mental health issues. Our team will meet you in the community or at
-          home, school, or the office. Together, we'll develop a treatment plan
-          that will address your mental health needs, and we are confident that
-          you will achieve short- and long-term mental and behavioral health
-          goals.
-        </span>
+        <span className="text-[16px] text-gray-500">Our Services</span>
         {/* Services */}
-        <div className="w-[800px]">
-
+        <div className="flex flex-col lg:grid lg:grid-cols-3 lg:gap-10 mt-6">
+          {serviceItems
+            .slice(0, viewAll ? serviceItems.length : 6)
+            .map((item, index) => (
+              <div key={index} className="flex flex-col mb-8">
+                <figure className="">
+                  <img
+                    src={item.img}
+                    alt=""
+                    className="w-[300px] h-[354px] object-cover shadow-md rounded-md"
+                  />
+                </figure>
+                <div className="flex flex-col items-center">
+                  <span className="mt-5">{item.desc01}</span>
+                  <span className="">{item.desc02}</span>
+                  <span className="">{item.desc03}</span>
+                </div>
+              </div>
+            ))}
         </div>
-
-        </div>
+        <Button
+          variant={"ghost"}
+          className="border border-[#262c36] text-[#262c36] mt-4 hover:bg-[#31bdc2] hover:border-none hover:text-[#f4f5f6]"
+          onClick={() => setViewAll(!viewAll)}
+        >
+          {!viewAll ? "View All" : "View Less"}
+        </Button>
       </div>
     </section>
   );
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,6 +1,74 @@
+import ServiceImg01 from "../assets/images/img01.jpg";
+import ServiceImg02 from "../assets/images/img02.jpg";
+import ServiceImg03 from "../assets/images/img03.jpg";
+
 export const navItems = [
   { label: "Services", href: "#" },
   { label: "Our Team", href: "#" },
   { label: "Referrals", href: "#" },
   { label: "Contact Us", href: "#" },
+];
+
+type serviceType = {
+  img: string;
+  desc01: string;
+  desc02: string;
+  desc03: string;
+};
+
+export const serviceItems: serviceType[] = [
+  {
+    img: ServiceImg01,
+    desc01: "Behavioral Health Assessments",
+    desc02: "Community Resources",
+    desc03: "Community Support",
+  },
+  {
+    img: ServiceImg02,
+    desc01: "Nursing Assessments & Health Services",
+    desc02: "Diagnostic Assessments",
+    desc03: "Psychiatric Treatment",
+  },
+  {
+    img: ServiceImg03,
+    desc01: "Psycho-Social Rehabilitation",
+    desc02: "Service Plan Development",
+    desc03: "Psychological Testing",
+  },
+  {
+    img: ServiceImg01,
+    desc01: "Family, Group & Individual Counseling",
+    desc02: "Case Management",
+    desc03: "",
+  },
+  {
+    img: ServiceImg01,
+    desc01: "Addictive Disease Support Services",
+    desc02: "",
+    desc03: "",
+  },
+  {
+    img: ServiceImg01,
+    desc01: "Drug Treatment & Education Program",
+    desc02: "Community Transition Planning",
+    desc03: "",
+  },
+  {
+    img: ServiceImg01,
+    desc01: "Medical Insurance",
+    desc02: "",
+    desc03: "",
+  },
+  {
+    img: ServiceImg01,
+    desc01: "Additional Medical Services",
+    desc02: "",
+    desc03: "",
+  },
+  {
+    img: ServiceImg01,
+    desc01: "Crisis Services",
+    desc02: "",
+    desc03: "",
+  },
 ];

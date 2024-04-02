155c7401cad7a8cd108147a0d1640176cab5880e
kngtee <anthonyurah@gmail.com>
2024-04-02 16:57:35 +0100

    hotfix

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,7 +1,7 @@
 export const ComingSoon = () => {
   return (
     <section className="container h-fit">
-      <div className="flex items-center justify-center gap-5 w-full h-[250px] lg:h-[500px] bgImg background-tint rounded-xl mt-10 mb-10">
+      <div className="flex items-center justify-center gap-5 w-full h-[250px] lg:h-[500px] bgImg background-tint rounded-xl mt-10 mb-10 p-5">
         <h1 className="text-[50px] leading-[40px] lg:text-[100px] lg:leading-[90px] font-bold tracking-tight text-[#f4f5f6]">
           Coming Soon...
         </h1>
